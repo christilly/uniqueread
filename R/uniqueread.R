f2758e8596911e45888b5387b47fe281f5d0b8c0
christilly <156473204+christilly@users.noreply.github.com>
2025-04-08 15:34:37 -0400

    Created functions res_data, dist_hist
    Created function res_data which calculates residuals average, standard deviation, highest, and lowest, as well as associated book titles.
    Created function dist_hist to display histogram of user positive/negative ratings.

--- R/uniqueread.R
+++ R/uniqueread.R
@@ -1,6 +1,7 @@
 #Required packages
 library("plyr")
 library("dplyr")
+library("ggplot2")
 
 
 #' Title: Read in and clean GoodReads data from CSV file
@@ -24,39 +25,91 @@ read_GR_data <- function(file_path) {
   return(data)
 }
 
-GR_data <- read_GR_data("/Users/tillytran/Desktop/sample_export.csv")
-
 #Testing
 #GR_data <- read_GR_data("/Users/tillytran/Desktop/goodreads_library_export.csv")
+GR_data <- read_GR_data("/Users/tillytran/Desktop/sample_export.csv")
 #GR_data <- read_GR_data("/Users/tillytran/Desktop/sample_export.csv")
 
 
-#Convert ratings of 0 in My.Rating to NA and remove those books
-#If a book has a rating of 0, it was not rated yet because Goodreads only allows users to give the lowest rating of 1, so these books should be removed
-GR_clean <- GR_data %>%
-  mutate(My.Rating = na_if(My.Rating, 0)) %>%  # Convert 0 to NA
-  filter(!is.na(My.Rating)) # Remove rows where My.Rating is NA
-GR_clean
+#' Title: Remove ratings of 0 from GR_data
+#'
+#' Description: If a book has a rating of 0, it was not rated yet because Goodreads
+#' only allows users to give the lowest rating of 1, so these books should be removed
+#'
+#' @param data_frame data frame with relevant Goodreads data.
+#' @return A data frame with ratings of 0 removed.
+#' @export
+clean_GR_ratings <- function(data_frame) {
+  data_frame %>%
+    dplyr::mutate(My.Rating = na_if(My.Rating, 0)) %>%
+    dplyr::filter(!is.na(My.Rating))
+}
+GR_clean <- clean_GR_ratings(GR_data)
 
-#calculate residuals of average rating - user rating
-GR_clean <- GR_clean %>%
-  mutate(residuals = My.Rating - Average.Rating)
 
-#Calculate average residual score
-avg_res <- mean(GR_clean$residuals)
-cat("On average, your book ratings differed from average Goodreads users by:", avg_res, "points")
+#' Title: Calculate residuals of the average Goodreads rating - user rating
+#'
+#' Description: Calculate the residuals of average Goodreads rating - user rating.
+#'
+#' @param data_frame cleaned data frame (GR_clean).
+#' @return a data frame with residuals.
+#' @export
+resids <- function(data_frame) {
+  data_frame %>%
+    dplyr::mutate(residuals = My.Rating - Average.Rating)
+}
+GR_clean <- resids(GR_clean)
 
-#Highest residual (greatest positive difference in user vs average rating)
-hr <- max(GR_clean$residuals)
-#Lowest residual (greatest negative difference in user vs average rating)
-lr <- min(GR_clean$residuals)
 
-#Print titles of highest and lowest residuals
-hr_title <- GR_clean$Title[which(GR_clean$residuals == hr)]
-lr_title <- GR_clean$Title[which(GR_clean$residuals == lr)]
-cat("The book you loved that everyone else hated:", hr_title, "with a rating difference of", hr, "stars", "\n")
-cat("The book you hated that everyone else loved:", lr_title, "with a rating difference of", lr, "stars", "\n")
 
+#' Title: Calculate average, standard deviation, highest, and lowest residuals
+#'
+#' Description: Using the residuals data in the data frame,
+#' calculate the average residual score, as well as the highest
+#' and lowest scores.
+#'
+#' @param data_frame data frame with residuals data.
+#' @return average, highest, and lowest residual data and titles associated with data.
+#' @export
+res_data <- function(data_frame) {
+  avg_res <- mean(data_frame$residuals, na.rm = TRUE)
+  sd_res <- sd(data_frame$residuals, na.rm = TRUE)
+  hr <- max(data_frame$residuals, na.rm = TRUE)
+  lr <- min(data_frame$residuals, na.rm = TRUE)
+
+  hr_title <- data_frame$Title[which(data_frame$residuals == hr)][1]
+  lr_title <- data_frame$Title[which(data_frame$residuals == lr)][1]
+
+  cat("On average, your book ratings differed from average Goodreads users by:",
+      round(avg_res, 4), "points with a standard deviation of", round(sd_res, 4),  "\n")
+  cat("The book you loved more than others:", hr_title,
+      "with a rating difference of", round(hr, 2), "stars\n")
+  cat("The book you hated more than others:", lr_title,
+      "with a rating difference of", round(lr, 2), "stars\n")
+}
+
+GR <- res_data(GR_clean)
+
+#' Title: Distribution histogram
+#'
+#' Description: Visually displays distribution histogram to see if user tends to be
+#' more positive or negative than average Goodreads users.
+#'
+#' @param data_frame data frame with residuals data.
+#' @return A distribution histogram.
+#' @export
+dist_hist <- function(data_frame) {
+  ggplot(data = data_frame, aes(x = residuals)) +
+    geom_histogram(binwidth = 0.5, fill = "steelblue", color = "black") +
+    labs(
+      title = "Distribution of Rating Residuals",
+      x = "Residuals (My Rating - Average Rating)",
+      y = "Frequency"
+    ) +
+    theme_classic()
+}
+
+dist_hist(GR_clean)
 
 
 
